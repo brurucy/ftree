96680f1655699c1b236bb276954e192035fc757c
brurucy <brurucy@gmail.com>
2023-07-14 18:43:15 +0300

    v1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,11 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [Unreleased]
 ### Changed
 
+## [1.0.0] - 2023-07-14
+### Changed
+- all APIs
+- refactored all functions thanks to @matthieum
+
 ## [0.1.1] - 2023-07-13
 ### Changed
 - implementation of msb and lsb
--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,21 @@
 #![cfg_attr(not(test), no_std)]
 extern crate alloc;
 use alloc::vec::Vec;
+use core::ops::{AddAssign, SubAssign};
 
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 
-const fn least_significant_bit(idx: usize) -> usize {
-    return idx & idx.wrapping_neg();
-}
-
-const fn most_significant_bit(idx: usize) -> usize {
-    if idx == 0 {
-        return 0;
-    }
-    return 1 << (usize::BITS - 1 - idx.leading_zeros());
-}
-
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Clone, PartialEq)]
-pub struct FenwickTree {
-    inner: Vec<usize>,
+pub struct FenwickTree<T> {
+    inner: Vec<T>,
 }
 
-impl FromIterator<usize> for FenwickTree {
-    fn from_iter<T: IntoIterator<Item=usize>>(iter: T) -> Self {
-        return FenwickTree::new(iter, |val| *val)
-    }
-}
-
-impl<const N: usize> From<[usize; N]> for FenwickTree
+impl<T> FromIterator<T> for FenwickTree<T>
+where
+    T: Copy + AddAssign,
 {
-    fn from(value: [usize; N]) -> Self {
-        return FenwickTree::from_iter(value.into_iter());
-    }
-}
-
-impl FenwickTree {
     /// Creates a new fenwick tree.
     ///
     /// # Examples
@@ -46,28 +26,61 @@ impl FenwickTree {
     /// let lengths: [usize; 5] = [1, 6, 3, 9, 2];
     /// // This is how lengths fenwick tree will look like internally
     /// let fenwick_tree: Vec<usize> = vec![1, 7, 3, 19, 2];
-    /// // And this is how it can be constructed with new
-    /// let initialized_fenwick_tree: FenwickTree = FenwickTree::new(lengths, |length| *length);
+    /// // And this is how it can be constructed
+    /// let initialized_fenwick_tree = FenwickTree::from_iter(lengths);
     /// ```
-    pub fn new<T>(container: impl IntoIterator<Item = T>, f: impl Fn(&T) -> usize) -> Self {
-        let mut inner: Vec<usize> = container.into_iter().map(|value| f(&value)).collect();
+    fn from_iter<I>(iter: I) -> Self
+    where
+        I: IntoIterator<Item = T>,
+    {
+        let mut inner: Vec<T> = iter.into_iter().collect();
         let n = inner.len();
 
         for i in 0..n {
             let parent = i | (i + 1);
             if parent < n {
-                inner[parent] += inner[i];
+                let child = inner[i];
+                inner[parent] += child;
             }
         }
 
         FenwickTree { inner }
     }
+}
+
+impl<const N: usize> From<[usize; N]> for FenwickTree<usize> {
+    fn from(value: [usize; N]) -> Self {
+        return FenwickTree::from_iter(value.into_iter());
+    }
+}
+
+impl<T> FenwickTree<T> {
+    /// Creates an empty(useless) fenwick tree.
+    ///
+    /// You might be looking for the method `from_iter` instead, which is the only
+    /// way to create a useful fenwick tree.
+    pub const fn new() -> Self {
+        let inner = Vec::new();
+
+        Self { inner }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.inner.is_empty()
+    }
+
+    pub fn len(&self) -> usize {
+        self.inner.len()
+    }
+}
+
+impl<T> FenwickTree<T> {
     /// Computes the prefix sum up until the desired index.
     ///
     /// The prefix sum up until the zeroth element is 0, since there is nothing before it.
     ///
     /// The prefix sum up until an index larger than the length is undefined, since every
-    /// element after the length - 1 is undefined.
+    /// element after the length - 1 is undefined, therefore it will panic.
     ///
     /// # Examples
     ///
@@ -75,32 +88,31 @@ impl FenwickTree {
     /// use ftree::FenwickTree;
     ///
     /// let lengths = [1, 6, 3, 9, 2];
-    /// let fenwick_array = FenwickTree::new(lengths, |item| *item);
+    /// let fenwick_array = FenwickTree::from_iter(lengths);
     ///
     /// let cases: Vec<(usize, usize)> =
-    ///  vec![(0, 0), (1, 1), (2, 7), (3, 10), (4, 19), (5, 21), (6, 0)];
+    ///  vec![(0, 0), (1, 1), (2, 7), (3, 10), (4, 19), (5, 21)];
     ///
     /// cases
     ///   .into_iter()
-    ///   .for_each(|(idx, expected_sum)| assert_eq!(fenwick_array.prefix_sum(idx), expected_sum))
+    ///   .for_each(|(idx, expected_sum)| assert_eq!(fenwick_array.prefix_sum(idx, 0), expected_sum))
     /// ```
-    pub fn prefix_sum(&self, index: usize) -> usize {
-        let mut sum = 0;
-        let mut current_idx = index;
+    pub fn prefix_sum(&self, index: usize, mut sum: T) -> T
+    where
+        T: Copy + AddAssign,
+    {
+        assert!(index < self.inner.len() + 1);
 
-        if index > self.inner.len() {
-            return sum;
-        }
+        let mut current_idx = index;
 
         while current_idx > 0 {
             sum += self.inner[current_idx - 1];
             current_idx &= current_idx - 1
         }
 
-        return sum;
+        sum
     }
-    /// Either increments, or decrements, the fenwick tree at a given index. Take care to not render
-    /// it invalid.
+    /// Increments a given index with a difference.
     ///
     /// # Examples
     ///
@@ -108,21 +120,54 @@ impl FenwickTree {
     /// use ftree::FenwickTree;
     ///
     /// let lengths = [1, 6, 3, 9, 2];
-    /// let mut fenwick_array = FenwickTree::new(lengths, |item| *item);
+    /// let mut fenwick_array = FenwickTree::from_iter(lengths);
     ///
-    /// let cases: Vec<(usize, usize)> = vec![(0, 0), (1, 2), (2, 8), (3, 11), (4, 20), (5, 22), (6, 0)];
+    /// let cases: Vec<(usize, usize)> = vec![(0, 0), (1, 2), (2, 8), (3, 11), (4, 20), (5, 22)];
     ///
-    /// fenwick_array.update_index(0, 1);
+    /// fenwick_array.add_at(0, 1);
     ///
     /// cases
     ///   .into_iter()
-    ///   .for_each(|(idx, expected_sum)| assert_eq!(fenwick_array.prefix_sum(idx), expected_sum))
+    ///   .for_each(|(idx, expected_sum)| assert_eq!(fenwick_array.prefix_sum(idx, 0), expected_sum))
     /// ```
-    pub fn update_index(&mut self, index: usize, diff: isize) {
+    pub fn add_at(&mut self, index: usize, diff: T)
+    where
+        T: Copy + AddAssign,
+    {
         let mut current_idx = index;
-        while current_idx < self.inner.len() {
-            self.inner[current_idx] = (self.inner[current_idx] as isize + diff) as usize;
-            current_idx |= current_idx + 1
+
+        while let Some(value) = self.inner.get_mut(current_idx) {
+            *value += diff;
+            current_idx |= current_idx + 1;
+        }
+    }
+    /// Subtracts a difference from a given index.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use ftree::FenwickTree;
+    ///
+    /// let lengths = [1, 6, 3, 9, 2];
+    /// let mut fenwick_array = FenwickTree::from_iter(lengths);
+    ///
+    /// let cases: Vec<(usize, usize)> = vec![(0, 0), (1, 0), (2, 6), (3, 9), (4, 18), (5, 20)];
+    ///
+    /// fenwick_array.sub_at(0, 1);
+    ///
+    /// cases
+    ///   .into_iter()
+    ///   .for_each(|(idx, expected_sum)| assert_eq!(fenwick_array.prefix_sum(idx, 0), expected_sum))
+    /// ```
+    pub fn sub_at(&mut self, index: usize, diff: T)
+    where
+        T: Copy + SubAssign,
+    {
+        let mut current_idx = index;
+
+        while let Some(value) = self.inner.get_mut(current_idx) {
+            *value -= diff;
+            current_idx |= current_idx + 1;
         }
     }
     /// Given a sum, finds the slot in which in which it would be "contained" within the original
@@ -134,7 +179,7 @@ impl FenwickTree {
     /// use ftree::FenwickTree;
     ///
     /// let lengths = [1, 6, 3, 9, 2];
-    /// let mut fenwick_array = FenwickTree::new(lengths, |item| *item);
+    /// let mut fenwick_array = FenwickTree::from_iter(lengths);
     ///
     /// let cases: Vec<(usize, usize)> = vec![(0, 0), (6, 1), (9, 2), (18, 3), (20, 4)];
     ///
@@ -142,66 +187,88 @@ impl FenwickTree {
     ///   .into_iter()
     ///   .for_each(|(prefix_sum, idx)| assert_eq!(fenwick_array.index_of(prefix_sum), idx))
     /// ```
-    pub fn index_of(&self, prefix_sum: usize) -> usize {
-        let length = self.inner.len();
-        let mut prefix_sum = prefix_sum;
-        let mut idx = 0;
-        let mut x = most_significant_bit(length) * 2;
-        while x > 0 {
-            let lsb = least_significant_bit(x);
+    pub fn index_of(&self, mut prefix_sum: T) -> usize
+    where
+        T: Copy + Ord + SubAssign,
+    {
+        let mut index = 0;
+        let mut probe = most_significant_bit(self.inner.len()) * 2;
+
+        while probe > 0 {
+            let lsb = least_significant_bit(probe);
             let half_lsb = lsb / 2;
-            if x <= length && self.inner[x - 1] < prefix_sum {
-                idx = x;
-                prefix_sum -= self.inner[x - 1];
-                x += half_lsb;
-            } else {
-                if lsb % 2 > 0 {
-                    break;
+            let other_half_lsb = lsb - half_lsb;
+
+            if let Some(value) = self.inner.get(probe - 1) {
+                if *value < prefix_sum {
+                    index = probe;
+                    prefix_sum -= *value;
+
+                    probe += half_lsb;
+                    continue;
                 }
-                x = x + half_lsb - lsb;
             }
+
+            if lsb % 2 > 0 {
+                break;
+            }
+
+            probe -= other_half_lsb;
         }
-        idx
+
+        index
+    }
+}
+
+const fn least_significant_bit(n: usize) -> usize {
+    n & n.wrapping_neg()
+}
+
+const fn most_significant_bit(n: usize) -> usize {
+    if n == 0 {
+        0
+    } else {
+        1 << (usize::BITS - 1 - n.leading_zeros())
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::FenwickTree;
     use alloc::vec;
     use alloc::vec::Vec;
-    use super::FenwickTree;
 
     #[test]
     fn test_new() {
         let lengths: [usize; 5] = [1, 6, 3, 9, 2];
         let expected_index: Vec<usize> = vec![1, 7, 3, 19, 2];
-        let actual_index = FenwickTree::new(lengths, |item| *item);
+        let actual_index = FenwickTree::from_iter(lengths);
         assert_eq!(expected_index, actual_index.inner)
     }
 
     #[test]
     fn test_prefix_sum() {
         let lengths = [1, 6, 3, 9, 2];
-        let fenwick_array = FenwickTree::new(lengths, |item| *item);
+        let fenwick_array = FenwickTree::from_iter(lengths);
 
         let cases: Vec<(usize, usize)> =
-            vec![(0, 0), (1, 1), (2, 7), (3, 10), (4, 19), (5, 21), (6, 0)];
+            vec![(0, 0), (1, 1), (2, 7), (3, 10), (4, 19), (5, 21)];
         // The prefix sum up until the zeroth element is 0, since there is nothing before it
         // The prefix sum up until an index larger than the length is undefined, since every
         // element after the length - 1 is undefined
         cases
             .into_iter()
-            .for_each(|(idx, expected_sum)| assert_eq!(fenwick_array.prefix_sum(idx), expected_sum))
+            .for_each(|(idx, expected_sum)| assert_eq!(fenwick_array.prefix_sum(idx, 0), expected_sum))
     }
 
     #[test]
     fn test_update_index() {
         let lengths = [1, 6, 3, 9, 2];
-        let mut fenwick_array = FenwickTree::new(lengths, |item| *item);
+        let mut fenwick_array = FenwickTree::from_iter(lengths);
 
         let cases: Vec<(usize, usize)> = vec![(0, 2), (1, 8), (2, 3), (3, 20), (4, 2)];
 
-        fenwick_array.update_index(0, 1);
+        fenwick_array.add_at(0, 1);
 
         cases
             .into_iter()
@@ -210,8 +277,8 @@ mod tests {
 
     #[test]
     fn test_index_of() {
-        let lengths: Vec<usize> = vec![1, 6, 3, 9, 2];
-        let fenwick_array = FenwickTree::new(lengths, |item| *item);
+        let lengths = [1, 6, 3, 9, 2];
+        let fenwick_array = FenwickTree::from_iter(lengths);
 
         let cases: Vec<(usize, usize)> = vec![(0, 0), (6, 1), (9, 2), (18, 3), (20, 4)];
 
